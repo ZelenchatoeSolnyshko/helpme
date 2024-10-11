982e32b1af9edc52eb39a76c2903b141a69ceccd
skroschina <sonyacat371@gmail.com>
2024-10-11 11:48:15 +0300

    22

--- src/main/java/org/softwaretechnologies/ArrayFunctions.java
+++ src/main/java/org/softwaretechnologies/ArrayFunctions.java
@@ -9,8 +9,16 @@ public final class ArrayFunctions {
      * Функция, меняющая порядок элементов в массиве array на обратный.
      * @param array массив, который будет перевернут.
      */
+
     public static void reverse(int[] array) {
+        for( int i = 0; i <= array.length / 2; i++)
+        {
+           int o = array[i];
+           array[i] = array[array.length - 1 - i];
+           array[array.length - 1 - i] = o;
+        }
         // TODO: реализуйте вышеуказанную функцию
+
     }
 
     /**
@@ -24,6 +32,21 @@ public final class ArrayFunctions {
      * @param matrix матрица, в которой столбцы будут заменены на строки.
      */
     public static void rotateMatrix(int[][] matrix) {
+        if(matrix.length != matrix[0].length)
+        {
+            System.out.println("You are stupid!!!!!!");
+        }
+        int n = matrix.length;
+        for(int i = 0; i <= n / 2; i++)
+        {
+            for (int j = 0; j <= n / 2; j++)
+            {
+                int o = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = o;
+            }
+        }
         // TODO: реализуйте вышеуказанную функцию
+
     }
 }
