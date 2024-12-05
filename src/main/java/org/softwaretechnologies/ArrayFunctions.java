5d8e723163d8a04063eaea1a4b8092d379a33fc4
ZelenchatoeSolnyshko <sonyacat371@gmail.com>
2024-12-05 20:36:26 +0300

    commit

--- src/main/java/org/softwaretechnologies/ArrayFunctions.java
+++ src/main/java/org/softwaretechnologies/ArrayFunctions.java
@@ -5,10 +5,7 @@ public final class ArrayFunctions {
     private ArrayFunctions() {
     }
 
-    /**
-     * Функция, меняющая порядок элементов в массиве array на обратный.
-     * @param array массив, который будет перевернут.
-     */
+
 
     public static void reverse(int[] array) {
         int l = array.length;
@@ -18,20 +15,10 @@ public final class ArrayFunctions {
            array[i] = array[l - 1 - i];
            array[l - 1 - i] = o;
         }
-        // TODO: реализуйте вышеуказанную функцию
 
     }
 
-    /**
-     * Функция, заменяющая строки матрицы на столбцы матрицы. Пример:
-       1  2  3     1  4  7
-       4  5  6     2  5  8
-       7  8  9     3  6  9
-     * Функция работает только с квадратными матрицами.
-     * Если матрица не квадратная, то выведете на экран сообщение:
-       Матрица не квадратная
-     * @param matrix матрица, в которой столбцы будут заменены на строки.
-     */
+
     public static void rotateMatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
@@ -49,7 +36,7 @@ public final class ArrayFunctions {
                 matrix[j][i] = o;
             }
         }
-        // TODO: реализуйте вышеуказанную функцию
+
 
     }
 }
